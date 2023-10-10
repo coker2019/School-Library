f0be8d27371eb137de011066fc6c06b1b062e18a
Maurice Coker <47523191+coker2019@users.noreply.github.com>
2023-10-10 18:54:16 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 require_relative 'nameable'
 
-class Person
+class Person < Nameable 
   attr_accessor :name, :age
   attr_reader :id
 
